78ec29d80e2555f6452fab184a0a09dbf9d090e1
tony3298 <tonydakhoul@yahoo.com>
2015-05-08 19:41:22 -0500

    First commit

--- TaxCalculator/ViewController.m
+++ TaxCalculator/ViewController.m
@@ -7,8 +7,20 @@
 //
 
 #import "ViewController.h"
+#import <Foundation/Foundation.h>
+#import <QuartzCore/QuartzCore.h>
 
 @interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextField *priceTextField;
+
+@property (weak, nonatomic) IBOutlet UIButton *calculateButton;
+
+@property (weak, nonatomic) IBOutlet UISegmentedControl *segmentedControl;
+
+@property (weak, nonatomic) IBOutlet UILabel *resultLabel;
+
+@property double caTax, chiTax, nyTax;
 
 @end
 
@@ -16,12 +28,66 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    self.caTax = .075;
+    self.chiTax = .0925;
+    self.nyTax = .045;
+
+    self.
+
+    self.calculateButton.layer.cornerRadius = 5;
+    self.calculateButton.clipsToBounds = YES;
+
+
+    self.resultLabel.layer.cornerRadius = 5;
+    self.resultLabel.clipsToBounds = YES;
+    self.resultLabel.textColor = [UIColor blackColor];
+    self.resultLabel.text = @"Result";
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)onCalculateButtonTapped:(id)sender {
+
+    double result;
+
+    if (self.priceTextField.text.length > 0) {
+
+        NSString *userValue = self.priceTextField.text;
+        NSNumberFormatter *f = [[NSNumberFormatter alloc] init];
+
+        if ([f numberFromString:userValue]) {
+
+            NSLog(@"This is a number");
+
+
+            double userValueDouble = self.priceTextField.text.doubleValue;
+
+            if (self.segmentedControl.selectedSegmentIndex == 0) {
+
+                result = userValueDouble + (self.caTax * userValueDouble);
+
+                NSLog(@"%g", result);
+
+                self.resultLabel.text = [NSString stringWithFormat:@"%g", result];
+
+            } else if (self.segmentedControl.selectedSegmentIndex == 1) {
+
+                result = userValueDouble + (self.chiTax * userValueDouble);
+
+                NSLog(@"%g", result);
+
+                self.resultLabel.text = [NSString stringWithFormat:@"%g", result];
+
+            } else if (self.segmentedControl.selectedSegmentIndex == 2) {
+
+                result = userValueDouble + (self.nyTax * userValueDouble);
+
+                NSLog(@"%g", result);
+
+                self.resultLabel.text = [NSString stringWithFormat:@"%g", result];
+            }
+        }
+    }
 }
 
 @end
